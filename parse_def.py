e505d047dea8105530f1cddbc85e9a2957f2259d
m_mitin <maximus405@gmail.com>
2022-08-13 22:27:08 +0300

    fixes and add new logic

--- parse_def.py
+++ parse_def.py
@@ -1,5 +1,6 @@
 import openpyxl
 import zipfile
+import re
 
 def parseXLSX(workbook_name, product_code_string):
     file_name = ['ru', 'en', 'ar', 'cs', 'de', 'es', 'fi', 'fr', 'it', 'ja', 'pl', 'pt_br', 'th', 'tr', 'vi', 'zh_cn',
@@ -8,6 +9,7 @@ def parseXLSX(workbook_name, product_code_string):
     product_code = product_code_string
     names_array = []
     description_array = []
+    package_content_array = []
     #sheets = workbook.sheetnames
     sheet = workbook.active
     counter = 0
@@ -20,6 +22,14 @@ def parseXLSX(workbook_name, product_code_string):
         for cell in row_2:
             description_array.append(str(cell.value))
 
+    for row_3 in sheet['B4':'S4']:
+        for cell in row_3:
+            string = str(cell.value)
+            string = string.replace("\r", "")
+            string = string.replace("\n", "")
+            new_str = ' '.join(f'<li>{word}</li>' for word in string.split())
+            package_content_array.append(new_str)
+
     for file in file_name:
         with open(r'blueprint.po', "r+", encoding="utf-8") as f:
             read_data = f.read()
@@ -27,6 +37,7 @@ def parseXLSX(workbook_name, product_code_string):
         read_data = read_data.replace('%product_code%', product_code)
         read_data = read_data.replace('%product_name%', names_array[counter])
         read_data = read_data.replace('%product_description%', description_array[counter])
+        read_data = read_data.replace('%package_content%', package_content_array[counter])
         my_file.write(read_data)
         my_file.close()
         counter += 1
@@ -38,3 +49,5 @@ def create_zip(product_code):
                      'zh_tw', 'ko']
         for filename in filenames:
             archive.write(f'{filename}.po')
+
+#parseXLSX('Excelsior_pckg_content.xlsx', 'test_new_1')
--- scriptNagibator.py
+++ scriptNagibator.py
@@ -1,6 +1,6 @@
 from parse_def import *
 
-xlsx_name = 'Excelsior.xlsx'
+xlsx_name = 'Excelsior_pckg_content.xlsx'
 product_code = 'test_new_1'
 
 parseXLSX(xlsx_name, product_code)
